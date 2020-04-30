06ac0d4799572b36e246fc4429a8fc96f0a263bc
Moésio Meneses <moesiomenesesf@gmail.com>
2020-04-29 22:59:04 -0300

    Updates

---
+++ precache-manifest.301ddf30cc39e2b2675bd3a8d644b1f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5007220c6d8eda4ec34900e2f1bc84ae",
+    "url": "/hanzo-app/index.html"
+  },
+  {
+    "revision": "187bafae5a86ebf1ab1e",
+    "url": "/hanzo-app/static/css/main.397c81e6.chunk.css"
+  },
+  {
+    "revision": "387dfcdf5e42b16aa333",
+    "url": "/hanzo-app/static/js/2.a6bb48bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hanzo-app/static/js/2.a6bb48bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187bafae5a86ebf1ab1e",
+    "url": "/hanzo-app/static/js/main.bdcb8854.chunk.js"
+  },
+  {
+    "revision": "94e6c348856efcb19751",
+    "url": "/hanzo-app/static/js/runtime-main.a754b621.js"
+  },
+  {
+    "revision": "ebdf61f62269296e96e6322b5dc74a15",
+    "url": "/hanzo-app/static/media/loading.ebdf61f6.svg"
+  },
+  {
+    "revision": "712c64300c0e946b0961143476789a10",
+    "url": "/hanzo-app/static/media/logo.712c6430.jpg"
+  }
+]);
